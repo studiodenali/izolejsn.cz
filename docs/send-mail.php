06a92cac37e1ba9249a6f81f807ef4d845eb53f5
davidbrzy <brzy@studiodenali.cz>
2022-09-03 18:45:06 +0200

    update send-mail.php, style.css and style.scss

--- docs/send-mail.php
+++ docs/send-mail.php
@@ -5,6 +5,7 @@ $to = 'admin@studiodenali.cz';
 $from = $_POST['email'];
 $subject = $_POST['subject'];
 $name = $_POST['name'];
+$tel = $_POST['tel'];
 $message = $_POST['message'];
 
 $headers = 'MIME-Version: 1.0' . '\r\n';
